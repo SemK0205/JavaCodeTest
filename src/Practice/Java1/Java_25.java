02cd4b78c68583ffbea749fd1dd525b9df9b0972
SemK0205 <s413625@gmail.com>
2025-06-16 20:38:41 +0900

    25.06.16

---
+++ src/Practice/Java1/Java_25.java
@@ -0,0 +1,31 @@
+package practice.java1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Java_25 {
+
+    /*
+    문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+    s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+
+    제한 사항
+    str은 길이 1 이상인 문자열입니다.
+     */
+
+    public String solution(String s) {
+        Character[] chars = new Character[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            chars[i] = s.charAt(i);
+        }
+
+        Arrays.sort(chars, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : chars) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
